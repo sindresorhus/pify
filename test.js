4471b7ac2d637ab06fa45e73a3b10b6dd21dee0f
Sindre Sorhus <sindresorhus@gmail.com>
2015-10-26 20:29:38 +0700

    ES2015ify tests

--- test.js
+++ test.js
@@ -1,36 +1,38 @@
-'use strict';
-var fs = require('fs');
-var test = require('ava');
-var pinkiePromise = global.Promise = require('pinkie-promise');
-var fn = require('./');
+import fs from 'fs';
+import test from 'ava';
+import pinkiePromise from 'pinkie-promise';
+import fn from './';
+
+global.Promise = pinkiePromise;
 
 function fixture(cb) {
-	setImmediate(function () {
+	setImmediate(() => {
 		cb(null, 'unicorn');
 	});
 }
 
 function fixture2(x, cb) {
-	setImmediate(function () {
+	setImmediate(() => {
 		cb(null, x);
 	});
 }
 
 function fixture3(cb) {
-	setImmediate(function () {
+	setImmediate(() => {
 		cb(null, 'unicorn', 'rainbow');
 	});
 }
 
 function fixture4(cb) {
-	setImmediate(function () {
+	setImmediate(() => {
 		cb(null, 'unicorn');
 	});
+
 	return 'rainbow';
 }
 
-fixture4.meow = function (cb) {
-	setImmediate(function () {
+fixture4.meow = cb => {
+	setImmediate(() => {
 		cb(null, 'unicorn');
 	});
 };
@@ -39,59 +41,45 @@ function fixture5() {
 	return 'rainbow';
 }
 
-var fixtureModule = {
+const fixtureModule = {
 	method1: fixture,
 	method2: fixture,
 	method3: fixture5
 };
 
-test('main', function (t) {
+test('main', async t => {
 	t.is(typeof fn(fixture)().then, 'function');
-
-	return fn(fixture)().then(function (data) {
-		t.is(data, 'unicorn');
-	});
+	t.is(await fn(fixture)(), 'unicorn');
 });
 
-test('pass argument', function (t) {
-	return fn(fixture2)('rainbow').then(function (data) {
-		t.is(data, 'rainbow');
-	});
+test('pass argument', async t => {
+	t.is(await fn(fixture2)('rainbow'), 'rainbow');
 });
 
-test('custom Promise module', function (t) {
-	return fn(fixture, pinkiePromise)().then(function (data) {
-		t.is(data, 'unicorn');
-	});
+test('custom Promise module', async t => {
+	t.is(await fn(fixture, pinkiePromise)(), 'unicorn');
 });
 
-test('multiArgs option', function (t) {
-	return fn(fixture3, {multiArgs: true})().then(function (data) {
-		t.same(data, ['unicorn', 'rainbow']);
-	});
+test('multiArgs option', async t => {
+	t.same(await fn(fixture3, {multiArgs: true})(), ['unicorn', 'rainbow']);
 });
 
-test('wrap core method', function (t) {
-	return fn(fs.readFile)('package.json').then(function (data) {
-		t.is(JSON.parse(data).name, 'pify');
-	});
+test('wrap core method', async t => {
+	t.is(JSON.parse(await fn(fs.readFile)('package.json')).name, 'pify');
 });
 
-test('module support', function (t) {
-	return fn(fs).readFile('package.json').then(function (data) {
-		t.is(JSON.parse(data).name, 'pify');
-	});
+test('module support', async t => {
+	t.is(JSON.parse(await fn(fs).readFile('package.json')).name, 'pify');
 });
 
-test('module support - doesn\'t transform *Sync methods by default', function (t) {
-	var data = fn(fs).readFileSync('package.json');
-	t.is(JSON.parse(data).name, 'pify');
+test('module support - doesn\'t transform *Sync methods by default', t => {
+	t.is(JSON.parse(fn(fs).readFileSync('package.json')).name, 'pify');
 	t.end();
 });
 
-test('module support - preserves non-function members', function (t) {
-	var module = {
-		method: function () {},
+test('module support - preserves non-function members', t => {
+	const module = {
+		method: () => {},
 		nonMethod: 3
 	};
 
@@ -99,8 +87,8 @@ test('module support - preserves non-function members', function (t) {
 	t.end();
 });
 
-test('module support - transforms only members in opions.include', function (t) {
-	var pModule = fn(fixtureModule, {
+test('module support - transforms only members in opions.include', t => {
+	const pModule = fn(fixtureModule, {
 		include: ['method1', 'method2']
 	});
 
@@ -110,8 +98,8 @@ test('module support - transforms only members in opions.include', function (t) 
 	t.end();
 });
 
-test('module support - doesn\'t transform members in opions.exclude', function (t) {
-	var pModule = fn(fixtureModule, {
+test('module support - doesn\'t transform members in opions.exclude', t => {
+	const pModule = fn(fixtureModule, {
 		exclude: ['method3']
 	});
 
@@ -121,8 +109,8 @@ test('module support - doesn\'t transform members in opions.exclude', function (
 	t.end();
 });
 
-test('module support - options.include over opions.exclude', function (t) {
-	var pModule = fn(fixtureModule, {
+test('module support - options.include over opions.exclude', t => {
+	const pModule = fn(fixtureModule, {
 		include: ['method1', 'method2'],
 		exclude: ['method2', 'method3']
 	});
@@ -133,20 +121,20 @@ test('module support - options.include over opions.exclude', function (t) {
 	t.end();
 });
 
-test('module support — function modules', function (t) {
-	var pModule = fn(fixture4);
+test('module support — function modules', t => {
+	const pModule = fn(fixture4);
 
 	t.is(typeof pModule().then, 'function');
 	t.is(typeof pModule.meow().then, 'function');
 	t.end();
 });
 
-test('module support — function modules exclusion', function (t) {
-	var pModule = fn(fixture4, {
+test('module support — function modules exclusion', t => {
+	const pModule = fn(fixture4, {
 		excludeMain: true
 	});
 
 	t.is(typeof pModule.meow().then, 'function');
-	t.not(typeof pModule(function () {}).then, 'function');
+	t.not(typeof pModule(() => {}).then, 'function');
 	t.end();
 });
