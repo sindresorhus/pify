b6fb7bb5ccb42ca7402f7f9e361ac36320b529a6
Rob Garrison <wowmotty@gmail.com>
2018-02-27 14:45:48 -0600

    Throw on invalid input. Fixes #58

--- test.js
+++ test.js
@@ -50,6 +50,17 @@ test('main', async t => {
 	t.is(await m(fixture)(), 'unicorn');
 });
 
+test('throw error on invalid input', t => {
+	let error = t.throws(() => m());
+	t.is(error.message, 'Invalid object type "undefined"');
+
+	error = t.throws(() => m(''));
+	t.is(error.message, 'Invalid object type "string"');
+
+	error = t.throws(() => m(null));
+	t.is(error.message, 'Invalid object type "null"');
+});
+
 test('error', async t => {
 	t.is(await m(fixture1)().catch(err => err), 'error');
 });
