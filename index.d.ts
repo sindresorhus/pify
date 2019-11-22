7e2590ea5073018247769794755542fb6042c3a6
Xianming Zhong <chinesedfan@qq.com>
2019-11-22 20:28:32 +0800

    Use ts-toolbelt

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,5 @@
+import { Tuple } from 'ts-toolbelt';
+
 interface PifyOptions {
     multiArgs?: boolean,
     include?: Array<string | RegExp>,
@@ -7,8 +9,14 @@ interface PifyOptions {
     promiseModule?: PromiseConstructor
 }
 
-declare function pify(input: Function, options?: PifyOptions & { multiArgs: true }): (...args: unknown[]) => Promise<Array<unknown>>;
-declare function pify<T extends unknown>(input: Function, options?: PifyOptions): (...args: unknown[]) => Promise<T>;
+type LastParameter<F extends (...args: any) => any> = Tuple.Last<Parameters<F>>;
+type CallbackFunction<F extends (...args: any) => any> =
+    LastParameter<F> extends (...args: any) => any ? LastParameter<F> : never;
+
+declare function pify<F extends (...args: any) => any>(input: F, options?: PifyOptions & { multiArgs: true, errorFirst: false }): (...args: Tuple.Pop<Parameters<F>>) => Promise<Parameters<CallbackFunction<F>>>;
+declare function pify<F extends (...args: any) => any>(input: F, options?: PifyOptions & { multiArgs: true }): (...args: Tuple.Pop<Parameters<F>>) => Promise<Tuple.Tail<Parameters<CallbackFunction<F>>>>;
+declare function pify<F extends (...args: any) => any>(input: F, options?: PifyOptions & { errorFirst: false }): (...args: Tuple.Pop<Parameters<F>>) => Promise<Parameters<CallbackFunction<F>>[0]>;
+declare function pify<F extends (...args: any) => any>(input: F, options?: PifyOptions): (...args: Tuple.Pop<Parameters<F>>) => Promise<Parameters<CallbackFunction<F>>[1]>;
 declare function pify(input: {[key: string]: unknown}, options?: PifyOptions): {[key: string]: any};
 
 export = pify;
--- index.test-d.ts
+++ index.test-d.ts
@@ -4,7 +4,11 @@ import pify = require('.');
 
 declare function request(
     url: string,
-    callback: (error?: Error, response?: unknown, body?: unknown) => void
+    callback: (error?: Error, response?: { statusCode: number }, body?: string) => void
+): void;
+declare function request2(
+    url: string,
+    callback: (response?: { statusCode: number }, body?: string) => void
 ): void;
 
 expectError(pify());
@@ -13,9 +17,10 @@ expectError(pify('abc'));
 expectError(pify([]));
 
 expectAssignable<Buffer>(await pify(fs).readFile('a.txt'))
-expectType<Array<unknown>>(await pify(request, { multiArgs: true })('https://www.google.com'))
+expectType<[{ statusCode: number }?, string?]>(await pify(request, { multiArgs: true })('https://www.google.com'))
+expectType<[{ statusCode: number }?, string?]>(await pify(request2, { multiArgs: true, errorFirst: false })('https://www.google.com'))
 expectAssignable<Buffer>(await pify(fs, { include: ['read'] }).readFile('a.txt'))
 expectAssignable<typeof fs.readFile>(pify(fs, { exclude: ['read'] }).readFile)
 expectAssignable<typeof request>(pify(request, { excludeMain: true }))
-expectAssignable<boolean>(await pify<boolean>(fs.exists, { errorFirst: false })('a.txt'))
-expectAssignable<Buffer>(await pify<Buffer>(fs.readFile, { promiseModule: Promise })('a.txt'))
+expectType<boolean>(await pify(fs.exists, { errorFirst: false })('a.txt'))
+expectType<Buffer>(await pify(fs.readFile, { promiseModule: Promise })('a.txt'))
