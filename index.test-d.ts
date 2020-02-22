70b33aa42857a37058ffdf09cdc5d68b96f0d450
Xianming Zhong <chinesedfan@qq.com>
2020-02-22 10:28:21 +0800

    Refactor tests

--- index.test-d.ts
+++ index.test-d.ts
@@ -1,27 +1,61 @@
 import {expectType, expectError, expectAssignable} from 'tsd';
-import * as fs from 'fs';
 import pify = require('.');
 
-declare function request(
-    url: string,
-    callback: (error?: Error, response?: { statusCode: number }, body?: string) => void
-): void;
-declare function request2(
-    url: string,
-    callback: (response?: { statusCode: number }, body?: string) => void
-): void;
+const fs = {
+    readFile(path: string, callback: (error?: Error, data?: Buffer) => void): void {
+        callback(undefined, new Buffer('abc'))
+    },
+    exists(path: string, callback: (result: boolean) => void): void {
+        callback(true)
+    }
+}
 
+// other types
 expectError(pify());
 expectError(pify(42));
 expectError(pify('abc'));
 expectType<number[]>(pify([1,2]));
 
-expectType<Promise<Buffer>>(pify(fs).readFile('a.txt'))
+// functions
+expectAssignable<() => Promise<never>>(pify(() => {}))
 expectType<Promise<never>>(pify(() => {})())
+
+expectAssignable<(path: string) => Promise<Buffer | undefined>>(pify(fs.readFile))
+expectType<Promise<Buffer | undefined>>(pify(fs.readFile)('a.txt'))
+
+declare function request(
+    url: string,
+    callback: (error?: Error, response?: { statusCode: number }, body?: string) => void
+): void;
+expectAssignable<(url: string) => Promise<[{ statusCode: number }?, string?]>>(pify(request, { multiArgs: true }))
 expectType<Promise<[{ statusCode: number }?, string?]>>(pify(request, { multiArgs: true })('https://www.google.com'))
+
+declare function request2(
+    url: string,
+    callback: (response?: { statusCode: number }, body?: string) => void
+): void;
+expectAssignable<(url: string) => Promise<[{ statusCode: number }?, string?]>>(pify(request2, { multiArgs: true, errorFirst: false }))
 expectType<Promise<[{ statusCode: number }?, string?]>>(pify(request2, { multiArgs: true, errorFirst: false })('https://www.google.com'))
-expectType<Promise<Buffer>>(pify(fs, { include: ['read'] }).readFile('a.txt'))
-expectAssignable<typeof fs.readFile>(pify(fs, { exclude: ['read'] }).readFile)
-expectAssignable<typeof request>(pify(request, { excludeMain: true }))
+
+expectAssignable<(path: string) => Promise<boolean>>(pify(fs.exists, { errorFirst: false }))
 expectType<Promise<boolean>>(pify(fs.exists, { errorFirst: false })('a.txt'))
-expectType<Promise<Buffer>>(pify(fs.readFile, { promiseModule: Promise })('a.txt'))
+
+expectAssignable<(path: string) => Promise<Buffer | undefined>>(pify(fs.readFile, { promiseModule: Promise }))
+expectType<Promise<Buffer | undefined>>(pify(fs.readFile, { promiseModule: Promise })('a.txt'))
+
+// objects
+expectType<number>(pify({k1: 123, k2: 'abc'}).k1)
+expectType<string>(pify({k1: 123, k2: 'abc'}).k2)
+
+expectAssignable<(path: string) => Promise<Buffer | undefined>>(pify(fs).readFile)
+expectType<Promise<Buffer | undefined>>(pify(fs).readFile('a.txt'))
+
+expectAssignable<(path: string) => Promise<Buffer | undefined>>(pify(fs, { include: ['read'] }).readFile)
+expectType<Promise<Buffer | undefined>>(pify(fs, { include: ['read'] }).readFile('a.txt'))
+
+expectAssignable<typeof fs.readFile>(pify(fs, { exclude: ['read'] }).readFile)
+expectType<void>(pify(fs, { exclude: ['read'] }).readFile('a.txt', (error?: Error, data?: Buffer) => {}))
+
+// mixed
+expectAssignable<typeof fs>(pify(fs))
+expectAssignable<typeof fs>(pify(fs, { excludeMain: true }))
