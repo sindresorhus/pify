da65e27e84f72403710758b75c80e0ab63ddd37d
Tom Sherman <the.tomsherman@gmail.com>
2022-08-31 15:31:12 +0100

    Add errorFirst support

--- index.d.ts
+++ index.d.ts
@@ -9,22 +9,27 @@ type DropLast<T extends readonly unknown[]> = T extends [...(infer U), any]
 
 type StringEndsWith<S, X extends string> = S extends `${infer _}${X}` ? true : false;
 
-interface Options<Includes extends readonly unknown[], Excludes extends readonly unknown[], MultiArgs extends boolean = false> {
+interface Options<Includes extends readonly unknown[], Excludes extends readonly unknown[], MultiArgs extends boolean = false, ErrorFirst extends boolean = true> {
 	multiArgs?: MultiArgs;
 	include?: Includes;
 	exclude?: Excludes;
+	errorFirst?: ErrorFirst;
 }
 
-interface InternalOptions<Includes extends readonly unknown[], Excludes extends readonly unknown[], MultiArgs extends boolean = false> {
+interface InternalOptions<Includes extends readonly unknown[], Excludes extends readonly unknown[], MultiArgs extends boolean = false, ErrorFirst extends boolean = true> {
 	multiArgs: MultiArgs;
 	include: Includes;
 	exclude: Excludes;
+	errorFirst: ErrorFirst;
 }
 
-type Promisify<Args extends readonly unknown[], GenericOptions extends InternalOptions<readonly unknown[], readonly unknown[], boolean>> = (
+
+type Promisify<Args extends readonly unknown[], GenericOptions extends InternalOptions<readonly unknown[], readonly unknown[], boolean, boolean>> = (
 	...args: DropLast<Args>
-) => Last<Args> extends (...args: any) => any
-	? Promise<
+) =>
+Last<Args> extends (...args: any) => any
+	? Parameters<Last<Args>> extends [infer SingleCallbackArg] ? GenericOptions extends { errorFirst: true } ? Promise<unknown> : Promise<SingleCallbackArg>
+	: Promise<
 	GenericOptions extends {multiArgs: false}
 		? Last<Parameters<Last<Args>>>
 		: Parameters<Last<Args>>
@@ -34,6 +39,7 @@ type Promisify<Args extends readonly unknown[], GenericOptions extends InternalO
 type PromisifyModule<
 	Module extends Record<string, any>,
 	MultiArgs extends boolean,
+	ErrorFirst extends boolean,
 	Includes extends ReadonlyArray<keyof Module>,
 	Excludes extends ReadonlyArray<keyof Module>,
 > = {
@@ -44,7 +50,7 @@ type PromisifyModule<
 				? Module[K]
 				: StringEndsWith<K, 'Sync' | 'Stream'> extends true
 					? Module[K]
-					: Promisify<Args, InternalOptions<Includes, Excludes, MultiArgs>>
+					: Promisify<Args, InternalOptions<Includes, Excludes, MultiArgs, ErrorFirst>>
 		: Module[K];
 };
 
@@ -52,19 +58,21 @@ declare function pify<
 	FirstArg,
 	Args extends readonly unknown[],
 	MultiArgs extends boolean = false,
+	ErrorFirst extends boolean = true,
 >(
 	input: (arg: FirstArg, ...args: Args) => any,
-	options?: Options<[], [], MultiArgs>
-): Promisify<[FirstArg, ...Args], InternalOptions<[], [], MultiArgs>>;
+	options?: Options<[], [], MultiArgs, ErrorFirst>
+): Promisify<[FirstArg, ...Args], InternalOptions<[], [], MultiArgs, ErrorFirst>>;
 declare function pify<
 	Module extends Record<string, any>,
 	Includes extends ReadonlyArray<keyof Module> = [],
 	Excludes extends ReadonlyArray<keyof Module> = [],
 	MultiArgs extends boolean = false,
+	ErrorFirst extends boolean = true,
 >(
 	// eslint-disable-next-line unicorn/prefer-module
 	module: Module,
-	options?: Options<Includes, Excludes, MultiArgs>
-): PromisifyModule<Module, MultiArgs, Includes, Excludes>;
+	options?: Options<Includes, Excludes, MultiArgs, ErrorFirst>
+): PromisifyModule<Module, MultiArgs, ErrorFirst, Includes, Excludes>;
 
 export = pify;
